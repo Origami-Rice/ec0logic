eba92377d9cfa09b1f62390fd9ba6192bc40b281
Jenny <jzhang.yuyu@gmail.com>
2020-11-14 12:06:44 -0500

    added post route to foodlib.js

--- back-end/routes/foodlib.js
+++ back-end/routes/foodlib.js
@@ -7,6 +7,13 @@ router
         console.log('GET request to path /api/food-library');
         // TODO: Return all the foods in our library of "common food" items
         // Include their name and approximate shelf life in days, months, or years
+    })
+    .post(async (request, response) => {
+        console.log('POST request to path /api/food-library');
+        
+        // TODO: Add a new item to the library
+        // This item will have a name and shelf life passed in
+        
     }); 
 
 module.exports = router;
