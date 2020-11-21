f79b7086e7d25f9cc6d57e2d9462b8f59fe92ea8
Doreen Huang <doreen.huang@mail.utoronto.ca>
2020-11-20 22:02:22 -0500

    Fixed visual bugs and added styling for FoodSearchScreen

--- front-end/screens/FoodSearchScreen.js
+++ front-end/screens/FoodSearchScreen.js
@@ -1,77 +1,96 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
-
-import { TextInput } from 'react-native-paper';
-import { ListItem } from 'react-native-elements';
-
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import { ListItem } from "react-native-elements";
+import LibraryListItem from "../components/LibraryListItem";
 
 import send from "../requests/request.js";
 import endpoints from "../requests/endpoints.js";
+
+let customFonts = {
+  Montserrat_400Regular: require("../fonts/Montserrat-Regular.ttf"),
+  Montserrat_500Medium: require("../fonts/Montserrat-Medium.ttf"),
+  Montserrat_600SemiBold: require("../fonts/Montserrat-SemiBold.ttf"),
+};
 
 export default class FoodSearchScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       search: "",
-      items: [], 
+      items: [],
       itemsFiltered: [],
-    }
+    };
   }
 
   componentDidMount() {
-      send("getFoodLibrary")
-      .then(response => response.json())
+    send("getFoodLibrary")
+      .then((response) => response.json())
       .then((json) => {
-          this.setState({ 
-            items: json,
-            itemsFiltered: json });
+        this.setState({
+          items: json,
+          itemsFiltered: json,
+        });
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Error getting food library");
         console.log(error);
-      })
+      });
   }
 
-  // Filter list items based on 
+  // Filter list items based on
   searchItem(textToSearch) {
     this.setState({
-      itemsFiltered: this.state.items.filter(i => i.name.toLowerCase().includes(textToSearch.toLowerCase())),
-      search: textToSearch
+      itemsFiltered: this.state.items.filter((i) =>
+        i.name.toLowerCase().includes(textToSearch.toLowerCase())
+      ),
+      search: textToSearch,
     });
-
   }
 
   getSearch = () => {
     alert(this.state.search);
-  }
+  };
 
   onSelectItem = (item) => {
-      // TODO: Needs to be implemented in Parent
-      alert("Item selected " + item.name);
-      const { setSelectedItem } = this.props;
-      setSearchItem(item);
-
-  }
+    // TODO: Needs to be implemented in Parent
+    alert("Item selected " + item.name);
+    const { setSelectedItem } = this.props;
+    setSearchItem(item);
+  };
 
   populateList = (i) => {
-      return this.state.itemsFiltered.map((item) => 
-        (
-          <LibraryListItem item={item.name}       
-          shelfLife={item.days} 
-          onClick={() => this.onSelectItem(item)} />
-        )
-        );
-  }
+    return this.state.itemsFiltered.map((item) => (
+      <LibraryListItem
+        item={item.name}
+        shelfLife={item.days}
+        onClick={() => this.onSelectItem(item)}
+      />
+    ));
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput 
-        placeholder="Search Common Foods" 
-        onChangeText={text => {this.searchItem(text)}} />
-          
-        {this.populateList()}
-          
+        <View style={styles.topButtonsContainer}>
+          <TouchableOpacity style={styles.cancelButton}>
+            <Text style={styles.cancelText}>x</Text>
+          </TouchableOpacity>
+        </View>
+        <TextInput
+          placeholder="Search Common Foods"
+          onChangeText={(text) => {
+            this.searchItem(text);
+          }}
+        />
+        <View style={styles.listContainer}>{this.populateList()}</View>
       </View>
     );
   }
@@ -80,18 +99,58 @@ export default class FoodSearchScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    backgroundColor: "#ffffff",
     padding: 8,
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  listContainer: {
+    paddingVertical: 5,
   },
-  modal: {
-    backgroundColor: "#FFFFFF",
-    margin: 100
-  }
+  inputFormat: {
+    height: 31,
+    backgroundColor: "#ffffff",
+    borderColor: "black",
+    borderBottomWidth: 1,
+    fontSize: 14,
+    padding: 5,
+    paddingLeft: 10,
+    margin: 10,
+    textAlign: "center",
+    fontFamily: "Montserrat_500Medium",
+    zIndex: 1,
+  },
+  topButtonsContainer: {
+    flexDirection: "row-reverse",
+    justifyContent: "space-between",
+  },
+  cancelText: {
+    textAlign: "center",
+    alignSelf: "center",
+    fontFamily: "Montserrat_400Regular",
+    fontSize: 14,
+    zIndex: 1,
+  },
+  cancelButton: {
+    width: 37,
+    height: 37,
+    borderRadius: 37,
+    borderWidth: 1,
+    justifyContent: "center",
+    alignSelf: "flex-end",
+    backgroundColor: "#ffffff",
+    margin: 25,
+    zIndex: 1,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgba(0,0,0, .5)",
+        shadowOffset: { height: 4, width: 0 },
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+      },
+      android: {
+        elevation: 3,
+      },
+    }),
+  },
 });
--- front-end/screens/InventoryInputScreen.js
+++ front-end/screens/InventoryInputScreen.js
@@ -9,11 +9,12 @@ import {
   Dimensions,
   Platform,
 } from "react-native";
+import Modal from "react-native-modal";
 import Constants from "expo-constants";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 import QuantityDropdown from "../components/QuantityDropdown";
-import DatePicker from './components/DatePicker'
+import DatePicker from "./components/DatePicker";
 
 import send from "../requests/request";
 
@@ -32,6 +33,7 @@ export default class InventoryInputScreen extends React.Component {
       quantity: 0,
       unitMeasure: "",
       expiryDate: new Date(),
+      visibleModal: 0,
     };
   }
   async _loadFontsAsync() {
@@ -49,22 +51,23 @@ export default class InventoryInputScreen extends React.Component {
       alert("Please enter item name.");
       return;
     }
-    
+
     if (this.state.quantity === 0 || this.state.unitMeasure === "") {
       alert("Some fields have not been filled correctly. Please review.");
       return;
     }
-    
+
     const newItem = {
-        name: this.state.name,
-        expiryDate: this.state.expiryDate,
-        quantity: this.state.quantity,
-        unitsOfMeasure: this.state.unitMeasure
+      name: this.state.name,
+      expiryDate: this.state.expiryDate,
+      quantity: this.state.quantity,
+      unitsOfMeasure: this.state.unitMeasure,
     };
 
-    this.props.navigation.navigate('List', 
-    {screen: "Inventory", params: {new_item: newItem} });
-
+    this.props.navigation.navigate("List", {
+      screen: "Inventory",
+      params: { new_item: newItem },
+    });
   };
 
   setQuantity = (value) => {
@@ -102,28 +105,55 @@ export default class InventoryInputScreen extends React.Component {
         }}
       >
         <View style={styles.container}>
-          <TouchableOpacity
-            style={styles.cancelButton}
-            onPress={() => this.props.navigation.goBack(null)}
+          <View
+            style={{
+              justifyContent: "flex-start",
+              width: Dimensions.get("window").width,
+            }}
           >
-            <Text style={styles.cancelText}>x</Text>
-          </TouchableOpacity>
-          <TextInput
-            style={styles.inputFormat}
-            placeholder="Enter New Food Item"
-            onChangeText={this.handleNameEntered}
-          />
-          <Text style={styles.label}>Quantity:</Text>
-          <QuantityDropdown
-            setParentQuantity={this.setQuantity}
-            setParentUnit={this.setUnit}
-          ></QuantityDropdown>
-          <Text style={styles.label}>Select Expiry Date:</Text>
-          <DatePicker 
-          setParentExpiry={this.setExpiryDate} 
-          defaultDate={this.state.expiryDate}/>
-          <Text style={styles.label}> {this.state.expiryDate.toString()} </Text>
-          <View style={{ zIndex: -1 }}>
+            <View style={styles.topButtonsContainer}>
+              <TouchableOpacity
+                style={styles.cancelButton}
+                onPress={() => this.setState({ visibleModal: 2 })}
+              >
+                <Text style={styles.cancelText}>s</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.cancelButton}
+                onPress={() => this.props.navigation.goBack(null)}
+              >
+                <Text style={styles.cancelText}>x</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.inputFormat}
+                placeholder="Enter New Food Item"
+                onChangeText={this.handleNameEntered}
+              />
+              <Text style={styles.label}>Quantity:</Text>
+              <QuantityDropdown
+                setParentQuantity={this.setQuantity}
+                setParentUnit={this.setUnit}
+              ></QuantityDropdown>
+              <Text style={styles.label}>Select Expiry Date:</Text>
+              <DatePicker
+                setParentExpiry={this.setExpiryDate}
+                defaultDate={this.state.expiryDate}
+              />
+              <Text style={styles.label}>
+                {" "}
+                {this.state.expiryDate.toString()}{" "}
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              justifyContent: "flex-end",
+              flex: 1,
+              zIndex: -1,
+            }}
+          >
             <TouchableOpacity
               style={styles.confirmButton}
               onPress={this.saveItem}
@@ -131,6 +161,24 @@ export default class InventoryInputScreen extends React.Component {
               <Text style={styles.confirmText}>Confirm</Text>
             </TouchableOpacity>
           </View>
+          <Modal
+            isVisible={this.state.visibleModal === 2}
+            style={styles.bottomModal}
+            avoidKeyboard={false}
+          >
+            {
+              <ScrollView
+                style={{
+                  height: Dimensions.get("window").height,
+                  width: Dimensions.get("window").width,
+                }}
+              >
+                <View style={styles.modal}>
+                  <FoodSearchScreen></FoodSearchScreen>
+                </View>
+              </ScrollView>
+            }
+          </Modal>
         </View>
       </ScrollView>
     );
@@ -139,15 +187,22 @@ export default class InventoryInputScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    height: Dimensions.get("window").height,
-    padding: 8,
     flex: 1,
     flexDirection: "column",
     justifyContent: "space-between",
     alignItems: "center",
     marginVertical: 5,
-    zIndex: 1,
     backgroundColor: "#ffffff",
+    zIndex: 1,
+  },
+  inputContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  topButtonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   cancelText: {
     textAlign: "center",
@@ -166,13 +221,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
     margin: 25,
     zIndex: 1,
-    // iOS shadow
-    shadowColor: "rgba(0,0,0, .5)",
-    shadowOffset: { height: 4, width: 0 },
-    shadowOpacity: 0.5,
-    shadowRadius: 2,
-    // Android shadow
-    elevation: 3,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgba(0,0,0, .5)",
+        shadowOffset: { height: 4, width: 0 },
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+      },
+      android: {
+        elevation: 3,
+      },
+    }),
   },
   inputFormat: {
     width: "80%",
@@ -193,8 +252,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     fontFamily: "Montserrat_500Medium",
     fontSize: 14,
-    marginBottom: 5,
-    marginTop: 50,
+    marginVertical: 5,
     zIndex: 1,
   },
   confirmText: {
@@ -211,7 +269,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignSelf: "center",
     backgroundColor: "#d8d8d8",
-    marginVertical: Dimensions.get("window").height * 0.2,
+    margin: 40,
     ...Platform.select({
       ios: {
         shadowColor: "rgba(0,0,0, .5)",
@@ -224,5 +282,15 @@ const styles = StyleSheet.create({
       },
     }),
     zIndex: 1,
+  },
+  bottomModal: {
+    justifyContent: "flex-end",
+    margin: 0,
+    height: Dimensions.get("window").height,
+  },
+  modal: {
+    backgroundColor: "white",
+    borderColor: "rgba(0, 0, 0, 0.1)",
+    height: Dimensions.get("window").height,
   },
 });
