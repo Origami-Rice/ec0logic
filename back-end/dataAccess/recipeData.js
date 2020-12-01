c24db3d898d47a7d3f8e109beccd887aead03554
Jamin Yang <jamin.yang@mail.utoronto.ca>
2020-12-01 18:51:09 -0500

    Spoonacular API: search query route

---
+++ back-end/dataAccess/recipeData.js
@@ -0,0 +1,7 @@
+const db = "test_wasteless";
+const users_collection = "test_users";
+const executeQuery = require('../utilities/mongoConnect').executeQuery;
+
+
+//////////////////// Recipe Queries ////////////////////
+
---
+++ back-end/routes/recipes.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
+
+const api_key = "1beca095b4c44b54b6861b16e89a2f63";
+
+// const {
+//     _functions
+// } = require('../dataAccess/userData');
+
+router
+    .route('/')
+    .post(async (request, res) => {
+        console.log('GET request to path /api/recipe');
+        const query = request.body.query;
+        const diet = request.body.diet;
+        const intolerances = request.body.intolerances;
+        // Log body variables
+        console.log(query);
+        console.log(diet);
+        console.log(intolerances);
+
+        const url = new URL("https://api.spoonacular.com/recipes/search")
+        const parameters = {apiKey = api_key, query: query, diet: diet, intolerances:intolerances, number: 50, sort: "popularity"};
+        Object.keys(parameters).forEach(param => url.searchParams.append(param, parameters[param]));
+
+
+        fetch(url)
+        .then((response) => {
+            return res
+            .status(200)
+            .json(response.json);
+        })
+        .then((data) => {
+            //console.log(data[0].usedIngredients[0]);
+            res.status(200).send(data);
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).send("500: Internal Server Error");
+        });
+
+
+    })
+module.exports = router;
+
+
+/*
+POST REQUEST:
+
+in body:
+
+{
+    'query': 'beef'
+    'diet': 'vegetarian'                //or ""
+    'intolerances': [peanut, gluten]     //or ""
+}
+
+*/
