edb3da1369c59045489912dfe0571c4d311b5ffb
Jamin Yang <jamin.yang@mail.utoronto.ca>
2020-12-01 19:23:55 -0500

    updated instructions for using search API

--- back-end/routes/recipes.js
+++ back-end/routes/recipes.js
@@ -65,3 +65,41 @@ If you dont have any preferences for diet/intolerances, substitute with ""
 
 // EMPTY DIET/INTOLERANCES QUERY EXAMPLES
 //{"query": "Pasta", "diet": "", "intolerances": ""}
+
+
+
+
+/* Returned response format: 
+e.g., search {"query": "Pasta", "diet": "", "intolerances": ""}
+returned:
+
+{
+    "results": [
+        {
+            "id": 535835,
+            "title": "Spaghetti Carbonara",
+            "readyInMinutes": 25,
+            "servings": 4,
+            "sourceUrl": "http://DAMNDELICIOUS.NET/2014/03/29/spaghetti-carbonara/",
+            "openLicense": 0,
+            "image": "Spaghetti-Carbonara-535835.jpg"
+        },
+        {
+            "id": 249325,
+            "title": "Chili Tortellini",
+            "readyInMinutes": 45,
+            "servings": 10,
+            "sourceUrl": "http://picky-palate.com/2013/04/10/chili-tortellini/",
+            "openLicense": 0,
+            "image": "Chili-Tortellini-249325.jpg"
+        }
+    ],
+    "baseUri": "https://spoonacular.com/recipeImages/",
+    "offset": 0,
+    "number": 20,
+    "totalResults": 839,
+    "processingTimeMs": 584,
+    "expires": 1607126693519,
+    "isStale": false
+}
+*/
