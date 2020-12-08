e3737e205bf64e14aa1b2800651b456cba208419
Doreen Huang <doreen.huang@mail.utoronto.ca>
2020-12-08 03:44:56 -0500

    Updated layout for LoginScreen

--- front-end/screens/LoginScreen.js
+++ front-end/screens/LoginScreen.js
@@ -4,12 +4,13 @@ import {
   StyleSheet,
   TouchableOpacity,
   TextInput,
+  Image,
   Dimensions,
   Platform,
 } from "react-native";
-import TextRegular from "../components/TextRegular";
 import TextMedium from "../components/TextMedium";
 import TextSemiBold from "../components/TextSemiBold";
+import { Ionicons } from "@expo/vector-icons";
 import { Colours } from "../constants/colours.js";
 import * as Font from "expo-font";
 
@@ -17,11 +18,15 @@ let customFonts = {
   Montserrat_500Medium: require("../fonts/Montserrat-Medium.ttf"),
 };
 
+// NOTE: show/hide password temporarily resets styling
 export default class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       fontsLoaded: false,
+      email: "",
+      password: "",
+      showPassword: false,
     };
   }
   async _loadFontsAsync() {
@@ -33,13 +38,57 @@ export default class LoginScreen extends React.Component {
     this._loadFontsAsync();
   }
 
+  getIcon = () => {
+    if (this.state.showPassword) {
+      return <Ionicons name="md-eye-off" size={24} color={Colours.tint} />;
+    } else {
+      return <Ionicons name="md-eye" size={24} color={Colours.tint} />;
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ justifyContent: "flex-start", flex: 1 }}></View>
         <View
-          style={{ justifyContent: "flex-end", zIndex: -1, marginBottom: 25 }}
+          style={{
+            justifyContent: "flex-start",
+            flex: 1,
+            alignItems: "center",
+          }}
         >
+          <Image
+            source={require("../assets/wasteless-logo.png")}
+            style={{ height: 83, width: 100, marginTop: 50 }}
+          />
+          <TextSemiBold style={styles.header} text={"Log In Below"} />
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.inputFormat}
+              placeholder="Email"
+              value={this.state.email}
+              onChangeText={(text) => this.setState({ email: text })}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.inputFormat}
+              placeholder="Password"
+              value={this.state.password}
+              secureTextEntry={this.state.showPassword}
+              textContentType={"password"}
+              onChangeText={(text) => this.setState({ password: text })}
+            />
+            <TouchableOpacity
+              style={{ width: 24, height: 35, justifyContent: "center" }}
+              onPress={() =>
+                this.setState({ showPassword: !this.state.showPassword })
+              }
+            >
+              {this.getIcon()}
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{ justifyContent: "flex-end", flex: 1, marginBottom: 25 }}>
           <TouchableOpacity
             style={styles.confirmButton}
             onPress={this.props.route.params?.onPress}
@@ -65,36 +114,35 @@ const styles = StyleSheet.create({
     zIndex: 1,
     backgroundColor: Colours.screenBackground,
   },
-  inputFormat: {
+  header: {
+    textAlign: "center",
+    fontSize: 28,
+    color: Colours.tint,
+    margin: 30,
+    marginTop: 15,
+  },
+  inputContainer: {
     width: Dimensions.get("window").width * 0.8,
-    height: 31,
+    height: 35,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
     backgroundColor: Colours.borderedComponentFill,
     borderColor: Colours.tint,
     borderBottomWidth: 1,
+    flex: 0,
+    marginVertical: 10,
+  },
+  inputFormat: {
+    width: Dimensions.get("window").width * 0.8 - 24,
+    height: 31,
     fontSize: 14,
     color: Colours.tint,
     padding: 5,
     paddingLeft: 10,
-    margin: 10,
-    textAlign: "center",
+    marginVertical: 10,
+    textAlign: "left",
     fontFamily: "Montserrat_500Medium",
-    zIndex: 1,
-  },
-  label: {
-    textAlign: "center",
-    alignSelf: "center",
-    fontSize: 14,
-    color: Colours.tint,
-    marginBottom: 5,
-    marginTop: 50,
-    zIndex: 1,
-  },
-  optional: {
-    textAlign: "center",
-    alignSelf: "center",
-    fontSize: 11,
-    marginVertical: 5,
-    color: Colours.notice,
     zIndex: 1,
   },
   confirmText: {
