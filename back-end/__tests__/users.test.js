1cb07ac5a3bf1a9cc5eba1e7f2d235fda542f83d
William Chu <william.chu001@gmail.com>
2020-12-11 23:33:24 -0500

    Fixed user tests to match code from previous group. Basic use cases.

--- back-end/__tests__/users.test.js
+++ back-end/__tests__/users.test.js
@@ -5,47 +5,84 @@ const {
     remove_user
 } = require('../dataAccess/userData');
 
-const name = "test";
+const name = "test21";
+const signupUrl = "/api/users/signup"
+const deleteUrl = "/api/users/deleteuser"
 
-afterAll(async () => {
+/*afterAll(async () => {
     // remove the test user from the database
     return await remove_user(name);
-});
+});*/
+
+const newUser = {
+    "username": name,
+    "password": "password",
+    "email": "anEmail@gmail.com",
+    "firstname": "firstName",
+    "lastname": "lastname"
+}
+
+const invalidUser1 = {
+    "username": "",
+    "password": "password",
+    "email": "anEmail@gmail.com",
+    "firstname": "firstName",
+    "lastname": "lastname"
+}
+
+const invalidUser2 = {
+    "password": "password",
+    "email": "anEmail@gmail.com",
+    "firstname": "firstName",
+    "lastname": "lastname"
+}
+
 
 describe("user creation", () => {
-    test("save valid new user to database", async () => {
+    test("sign user up to database", async () => {
         // send post request
         const response = await request
-            .post("/api/users/newuser")
+            .post(signupUrl)
             .set("Accept", "application/json")
-            .send({
-                username: name
-            })
-            // console.log(response);
-
-            expect(response.status).toBe(200);
-            // expect(response.statusCode).toBe(200); //not sure what the difference is?????????????????????????????????????????????????????????????????
-            expect(response.body).toEqual({ success: "user " + name + " added" });
+            .send(newUser);
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({"success": "user " + name + " signed up"});
     });
 
-    test("does not create user with no username", async () => {
+    test("does not create user with empty fields", async () => {
         const response = await request
-            .post("/api/users/newuser")
+            .post(signupUrl)
             .set("Accept", "application/json")
-            .send({
-            })
-            expect(response.status).toBe(500);
-            expect(response.body).toEqual({"error": "Internal server error"})
+            .send(invalidUser1);
+        expect(response.status).toBe(422);
+        expect(response.body).toEqual({"error": "One of user's information fields is an invalid input"})
     });
 
-    test("does not create user with blank username", async () => {
+    test("does not create user with missing fields", async () => {
         const response = await request
-            .post("/api/users/newuser")
+            .post(signupUrl)
             .set("Accept", "application/json")
-            .send({
-                "username": ""
-            })
-            expect(response.status).toBe(500);
-            expect(response.body).toEqual({"error": "Internal server error"})
+            .send(invalidUser2);
+        expect(response.status).toBe(422);
+        expect(response.body).toEqual({"error": "One of user's information fields is an invalid input"})
     });
+
+    test("does not create user if their desired username already exists in the database", async () => {
+        const response = await request
+            .post(signupUrl)
+            .set("Accept", "application/json")
+            .send(newUser);
+        expect(response.status).toBe(409);
+        expect(response.body).toEqual({"error": "User with this username already exists"})
+    });
+});
+
+describe("user deletion", () => {
+    test("Succesfully deletes an existing user", async () => {
+        const response = await request 
+            .post(deleteUrl)
+            .send({"username": name});
+        expect(response.status).toBe(200); 
+        expect(response.body).toEqual({"success": "user " + name + " deleted"});
+    })
 });
