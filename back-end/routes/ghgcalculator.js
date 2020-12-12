af1f555e7be1cba3146f682781f7c9bd1a25c3ba
William Chu <william.chu001@gmail.com>
2020-12-11 20:40:58 -0500

    Removing unused functions in ghgcalculator

--- back-end/routes/ghgcalculator.js
+++ back-end/routes/ghgcalculator.js
@@ -7,39 +7,11 @@ router.use(bodyParser.json());
 
 const CO2_KG_PER_FOOD_KG = 1.9;
 const LBS_PER_KG = 2.20462262185; // 1 kg = 2.20462262185 lbs
-const G_PER_KG = 1000; 
 const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
     "October", "November", "December"]; 
 const {
-    get_inventorylist,
-    get_shopping_list,
     get_entire_history
 } = require('../dataAccess/userData');
-
-/** 
- * Calculates ghg of a list of inventory or food items. Uses the old format where items have 
- * a weight attribute and quantity refers to the number of the item. 
- */
-function calculateCO2(foodList){   
-    var totalWeightInKg = 0; 
-    var listLength = foodList.length; 
-    for (var i = 0; i < listLength; i++){
-        var units = String(foodList[i].unitsOfMeasure);
-        if(typeof foodList[i].weight != "undefined" && typeof foodList[i].unitsOfMeasure ==
-        "string"){
-            var newItemCO2 = foodList[i].quantity * foodList[i].weight;    
-            if (units.toUpperCase() == "LBS"){  
-                newItemCO2 = newItemCO2 / LBS_PER_KG;   
-            } else if (units.toUpperCase() == "G"){
-                newItemCO2 = newItemCO2 / G_PER_KG;
-            } else if (!units.toUpperCase() == "KG"){
-                newItemCO2 = 0; 
-            }
-            totalWeightInKg += newItemCO2; 
-        }
-    }
-    return (Math.round(totalWeightInKg * CO2_KG_PER_FOOD_KG * 1000) / 1000).toFixed(3); 
-}
 
 /**
  * Takes in float and returns it rounded to 3 decimals. 
@@ -90,50 +62,6 @@ function getGHGBreakdown(wastedItems){
     }
     return {"months": months.reverse(), "kg": kg.reverse(), "lbs": lbs.reverse()}; 
 }
-
-router  //currently not being used 
-    .route('/inventory/:username')
-    .get(async (request, response) => {
-        console.log('GET request to path /api/ghgcalculator/inventory/:username');
-        console.log(request.params);
-        
-        const username = request.params.username;
-        try {
-            const result = await get_inventorylist(username);
-            if (result && result.inventory_list){
-                c02_kgs = calculateCO2(result.inventory_list)
-                return response
-                    .status(200)
-                    .json(c02_kgs);
-            } else {
-                return response.status(404).json({"error": "GHG of inventory couldn't be calculated"}); 
-            }
-        } catch (error) {
-            console.log(error); 
-        }
-    }); 
-
-router  //currently not being used 
-    .route('/shoppingList/:username')
-    .get(async (request, response) => {
-        console.log('GET request to path /api/ghgcalculator/shoppingList/:username');
-        console.log(request.params);
-        
-        const username = request.params.username;
-        try {
-            const result = await get_shopping_list(username);
-            if (result && result.shopping_list){
-                c02_kgs = calculateCO2(result.shopping_list)
-                return response
-                    .status(200)
-                    .json(c02_kgs);
-            } else {
-                return response.status(404).json({"error": "GHG of shopping list couldn't be calculated"}); 
-            }
-        } catch (error) {
-            console.log(error); 
-        }
-    }); 
     
 router
     .route('/history/:username')
